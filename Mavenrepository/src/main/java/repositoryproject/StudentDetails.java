d2e9eacd997a06759bc650d7080211dd8841718f
arsha <arsha@192.168.1.43>
2024-08-06 18:39:01 +0530

    student message

---
+++ Mavenrepository/src/main/java/repositoryproject/StudentDetails.java
@@ -0,0 +1,11 @@
+package repositoryproject;
+
+public class StudentDetails {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Name:Arsha Raj");
+System.out.println("Course:java");
+	}
+
+}
